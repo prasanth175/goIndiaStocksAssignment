687e29f0c7dcc0fd4eebac911cf5bd47e70e75c2
Prasanth <1022prasanthkumar@gmail.com>
2023-07-02 19:19:56 +0530

    new changes

--- components/FeaturedCompanies.js
+++ components/FeaturedCompanies.js
@@ -4,9 +4,9 @@ import {companiesImages} from './Data'
 
 const FeaturedCompanies = () => {
   return (
-    <div className='bg-white'>
+    <div className='bg-white w-screen'>
         <h1 className='hidden lg:block text-red-500 font-bold text-lg pl-3 pt-1 pb-1'>FEATURED COMPANIES</h1>
-        <div className='[&::-webkit-scrollbar]:hidden flex items-center overflow-x-scroll p-3 bg-blue-50'>
+        <div className='[&::-webkit-scrollbar]:hidden flex items-center overflow-x-scroll p-3 bg-blue-50 w-full'>
           {companiesImages.map((each) => 
            <img className='mr-7 border-2 border-red-500 xs:border-0 rounded-full xs:rounded-none flex items-center justify-center' key={each.id} src={each.imageUrl} alt='company-logo' height={110} width={110} />
           )}
